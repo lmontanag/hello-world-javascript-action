a4e91dadc6a2b0c852a7b38747f46a6daf9ff78f
Luis Montana <l.montana@elsevier.com>
2023-02-20 17:01:04 +0000

    use webpage example

--- .github/workflows/scripts/script.js
+++ .github/workflows/scripts/script.js
@@ -1,6 +1,3 @@
-const github = require("@actions/github");
 module.exports = ({github, context}) => {
-    // return context.payload.client_payload.value
-    const payload = JSON.stringify(github.context.payload, undefined, 2)
-    return payload
+    return context.payload.client_payload.value
 }
