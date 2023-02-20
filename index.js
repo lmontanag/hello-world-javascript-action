1ced328f185ae112ef95f3b6314ccb4446552ce7
Luis Montana <l.montana@elsevier.com>
2023-02-20 16:47:35 +0000

    test gh-actions

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const core = require('@actions/core');
 const github = require('@actions/github');
 
+const  Octokit  = require("@octokit/rest");
+
 try {
     // `who-to-greet` input defined in action metadata file
     const nameToGreet = core.getInput('who-to-greet');
@@ -10,6 +12,45 @@ try {
     // Get the JSON webhook payload for the event that triggered the workflow
     const payload = JSON.stringify(github.context.payload, undefined, 2)
     console.log(`The event payload: ${payload}`);
+
+
+    // const octokit = new Octokit({
+    //     auth: "secret123",
+    //     userAgent: 'myApp v1.2.3',
+    //     previews: ['jean-grey', 'symmetra'],
+    //     timeZone: 'Europe/Amsterdam',
+    //     baseUrl: 'https://api.github.com',
+    //     log: {
+    //         debug: () => {},
+    //         info: () => {},
+    //         warn: console.warn,
+    //         error: console.error
+    //     },
+    //     request: {
+    //         agent: undefined,
+    //         fetch: undefined,
+    //         timeout: 0
+    //     }
+    // })
+    //
+    // const commit = await octokit.rest.repos.getCommit({
+    //     owner: "owner",
+    //     repo: "repo",
+    //     ref: github.context.sha
+    // });
+    //
+    //
+    // const targets = []
+    //
+    // for (const file of commit.data.files){
+    //
+    //     targets.push({
+    //         filename: file.filename,
+    //         status: file.status
+    //     })
+    //
+    // }
+    // core.setOutput("targets", JSON.stringify(targets))
 
 } catch (error) {
     core.setFailed(error.message);
